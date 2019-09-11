9df3af6bd90d7ee1ddfd2d757b0fc22097389e29
Alisson Zimmer <alisson.zimmer9@gmail.com>
2019-09-11 20:46:33 -0300

    cretaing pieces and sorting player pieces

--- peca.c
+++ peca.c
@@ -2,16 +2,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-TppecaDomino* create ()
+int TOTAL_PIECES = 28;
+
+TppecaDomino* createPieces ()
 {
 	TppecaDomino *pieces = init();
 	int i, j;
 
 	for (i = 0; i < 7; i++) {
 		for (j = i; j < 7; j++) {
-			pieces = insertIni(pieces, i, j)
+			pieces = insertIni(pieces, i, j);
 		}
 	}
+
+	return pieces;
+}
+
+TppecaDomino* getPlayerPieces (TppecaDomino* pieces, int totPieces)
+{
+	TppecaDomino *sorted = (TppecaDomino *) malloc(sizeof(TppecaDomino));
+	TppecaDomino *availables = pieces;
+	TppecaDomino *last;
+	int i, count = 0;
+
+	while (count < totPieces) {
+		int pieceNumber = randomInt(TOTAL_PIECES - count);
+
+		for (i = 0; i <= pieceNumber; i++) {
+		printf("i: %d, pieceNumber: %d, i == pieceNumber: %d\n", i, pieceNumber, i == pieceNumber);
+			if (i == pieceNumber) {
+				sorted = availables;
+				printf("sorted->numberRight: %d, sorted->numberLeft: %d\n", sorted->numberRight, sorted->numberLeft);
+				availables = removeFromAvailables (pieces, last, availables);
+			} else {
+				last = availables;
+				availables = availables->right;
+			}
+
+		}
+
+		count++;
+	}
+
+	TOTAL_PIECES -= count;
+
+	return sorted;
+}
+
+TppecaDomino* removeFromAvailables (TppecaDomino *pieces, TppecaDomino *last, TppecaDomino *availables)
+{
+	TppecaDomino *all = pieces;
+
+	if (all == NULL) {
+		return pieces;
+	}
+
+	if (all->right == NULL && all->left == NULL) {
+		free(all);
+		return NULL;
+	}
+
+	TppecaDomino *ret;
+
+	// Exclusão no início
+	if (all->left == NULL) {
+		ret = all->right;
+		ret->left = NULL;
+		free(all);
+		return ret;
+	}
+
+	// Exclusão no final
+	if (all->right == NULL) {
+		all->left->right = NULL;
+		free(all);
+		return pieces;
+	}
+
+	// Exclusão no meio
+	all->left->right = all->right;
+	all->right->left = all->left;
+	free(all);
 
 	return pieces;
 }
@@ -30,4 +101,9 @@ TppecaDomino* insertIni (TppecaDomino* pieces, int right, int left)
 TppecaDomino* init ()
 {
 	return NULL;
+}
+
+int randomInt (int length)
+{
+	return rand() % length;
 }
--- peca.h
+++ peca.h
@@ -1,6 +1,8 @@
 #ifndef PECA_H
 #define PECA_H
 
+int TOTAL_PIECES;
+
 typedef struct _pecaDomino {
 	int numberRight;
 	int numberLeft;
@@ -8,8 +10,12 @@ typedef struct _pecaDomino {
 	struct _pecaDomino *left;
 } TppecaDomino;
 
+int randomInt (int length);
+
 TppecaDomino* init ();
-TppecaDomino* create ();
+TppecaDomino* createPieces ();
+TppecaDomino* getPlayerPieces (TppecaDomino* pieces, int totPieces);
 TppecaDomino* insertIni (TppecaDomino* pieces, int right, int left);
+TppecaDomino* removeFromAvailables (TppecaDomino *pieces, TppecaDomino *last, TppecaDomino *availables);
 
 #endif
